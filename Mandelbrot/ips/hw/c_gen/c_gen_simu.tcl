024ef882c17daed2f01e17b0ffdf1162582eb1e8
isaia.spinelli <isaia.spinelli@heig-vd.ch>
2021-05-18 16:12:08 +0200

    simulation de c_gen

---
+++ Mandelbrot/ips/hw/c_gen/c_gen_simu.tcl
@@ -0,0 +1,20 @@
+# source D:/Master/S2/LPSC/Pratique/Projet/Mandelbrot/ips/hw/c_gen/c_gen_simu.tcl
+
+
+restart
+
+add_force {/c_gen/ClkxC} -radix hex {0 0ns} {1 5ns} -repeat_every 10ns
+add_force {/c_gen/RstxRA} -radix hex {1 0ns}
+
+add_force {/c_gen/ZoomInxSI} -radix hex {0 0ns}
+add_force {/c_gen/ZoomOutxSI} -radix hex {0 0ns}
+
+
+run 20 ns
+
+
+
+add_force {/c_gen/RstxRA} -radix hex {0 0ns}
+
+# 0x3ff = 1023 => 1024 pixel
+run 10300 ns
